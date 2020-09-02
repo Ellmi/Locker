3cf68cdeba8e3d06f35b5c77981c597f59393114
Yanmin XING <yanmin@Yanmins-MacBook-Pro.local>
2020-09-02 21:24:59 +0800

    refactor to remove no use attribute in LockerTicket

--- src/main/java/cn/xpbootcamp/locker/LockerTicket.java
+++ src/main/java/cn/xpbootcamp/locker/LockerTicket.java
@@ -1,12 +1,4 @@
 package cn.xpbootcamp.locker;
 
-import java.util.UUID;
-
 public class LockerTicket {
-    private UUID id;
-
-    public LockerTicket() {
-        this.id = UUID.randomUUID();
-    }
-
 }
